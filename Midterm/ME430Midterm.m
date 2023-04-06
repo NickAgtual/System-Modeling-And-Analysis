3dcfc386ca531132d8cc5891e3d92886840b5ecd
Nick Agtual <nick.agtual@icloud.com>
2023-04-06 10:50:20 -0700

    Changed function to script

--- Midterm/ME430Midterm.m
+++ Midterm/ME430Midterm.m
@@ -1,53 +1,63 @@
-function electromechanicalSystemSolver()
-
-%% Solving ODE
+%% Question 1
 
 % Defining time vector
 time = 0:.01:6500;
 
 % Solving ODE using ode45
-[T, alphas] = ode45(@electromechanicalODEs, time, [0 0 0]);
+[T, m] = ode45(@electromechanicalODEs, time, [0 0 0]);
 
 % Extracting constants in ODE fxn
-[~, constants] = electromechanicalODEs(T, alphas);
+[~, constants] = electromechanicalODEs(T, m);
 
-%% Plotting Rotation Angle
 % Creating new figure
 figure(1)
 
 % Plotting rotation angle vs. time
-plot(T, alphas(:, 1), 'displayName', 'Roation Angle')
+plot(T, m(:, 1) * (180 / pi), 'displayName', 'Roation Angle')
 
 % Plot parameters
 grid on
 grid minor
+ylim([0 76]);
 
 % Plot descriptors
 title('\emph{$\alpha$ vs. t}', 'fontsize', ...
     16, 'Interpreter', 'Latex')
 xlabel('\emph{t [sec]}', 'fontsize', 14, 'Interpreter', 'Latex')
-ylabel('\emph{$\alpha$}',...
+ylabel('\emph{$\alpha$ [deg]}',...
     'fontsize', 14, 'Interpreter', 'Latex')
 legend('location', 'northwest', 'Interpreter', 'Latex')
 
-%% Plotting Actuator Current
 % Creating new figure
 figure(2)
 
 % Plotting actuator current vs. time
-plot(T, alphas(:, 3), 'displayName', 'Actuator Current')
+plot(T, m(:, 3), 'displayName', 'Actuator Current')
 
 % Plot parameters
 grid on
 grid minor
 
 % Plot descriptors
-% Plot descriptors
 title('\emph{i vs. t}', 'fontsize', ...
     16, 'Interpreter', 'Latex')
 xlabel('\emph{t [sec]}', 'fontsize', 14, 'Interpreter', 'Latex')
-ylabel('\emph{i}',...
+ylabel('\emph{i [Amperes]}',...
     'fontsize', 14, 'Interpreter', 'Latex')
 legend('location', 'northwest', 'Interpreter', 'Latex')
 
-end
+%% Question 2
+
+% Definign symbolic variables
+syms ii a
+
+% Equillibrium equation #1
+eqEqn1 = 0 == (constants.k * ii) + ...
+    ((a ^ .3) * exp(-.05 * (a ^ .5)));
+
+% Equillibrium equation #2
+eqEqn2 = 0 == (d3 - ii) / d4;
+
+% Solving system of equations
+[A, B] = equationsToMatrix([eqEqn1, eqEqn2], [ii, a]);
+[ii, a] = linsolve(A, B)
